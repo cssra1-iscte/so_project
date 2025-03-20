428965ee67eccde08cacf05cef56a35a83618107
cssra1 <a82442@iscte.pt>
2025-03-20 18:11:27 +0000

    Nova versão ficheiro

--- aula-05/exemplo-2.1.c
+++ aula-05/exemplo-2.1.c
@@ -4,7 +4,9 @@ int main () {
 	float r;
 	int v[4] = {2, 6, -9, 78};
 	r = (float)a / b;
-	printf("A divisão de %d por %d é %f\n", a, b, r );
+	printf("A divisão de %5d por %05d é %05.2f\n", a, b, r );
 	int x = (b > 2);
 	printf("valor de x : %d\n", x);
+	// mostrar a 3ª posição do array v[] --> v[2]
+	printf("valor de v[2] é %d\n", v[2]);
 }
