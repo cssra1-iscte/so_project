7a11a8f5708a8657460d26fc702d60595b4e3fa0
cssra1 <a82442@iscte.pt>
2025-05-25 20:06:41 +0100

    Cliente terminado com sucesso --> falta colocar comments

--- parte-3/cliente.c
+++ parte-3/cliente.c
@@ -52,7 +52,15 @@ int main () {
 void c1_1_GetMsgQueue(key_t ipcKey, int *pmsgId) {
     so_debug("< [@param ipcKey:0x0%x]", ipcKey);
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    // tenta abrir a message queue já criada pelo servidor
+    int msgid = msgget(ipcKey, 0666); 
+    if (msgid == -1) {
+        so_error("C1.1", "Erro ao abrir a Message Queue do projeto");
+        exit(1);
+    }
+    *pmsgId = msgid;
+    so_success("C1.1", "Message Queue aberta com sucesso");
+
 
     so_debug("> [@return *pmsgId:%d]", *pmsgId);
 }
@@ -63,7 +71,15 @@ void c1_1_GetMsgQueue(key_t ipcKey, int *pmsgId) {
 void c1_2_ArmaSinaisCliente() {
     so_debug("<");
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    if (signal(SIGINT, c6_TrataCtrlC) == SIG_ERR) {
+        so_error("C1.2", "Erro ao armar o sinal SIGINT");
+        exit(1);
+    }
+    if (signal(SIGALRM, c7_TrataAlarme) == SIG_ERR) {
+        so_error("C1.2", "Erro ao armar o sinal SIGALRM");
+        exit(1);
+    }
+    so_success("C1.2", "Sinais armados com sucesso");
 
     so_debug(">");
 }
@@ -75,8 +91,50 @@ void c1_2_ArmaSinaisCliente() {
 void c2_1_InputEstacionamento(MsgContent *pclientRequest) {
     so_debug("<");
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    char buffer[100];
+    int valido = 1;
 
+    // Matrícula
+    do {
+        printf("Matrícula: ");
+        if (!fgets(buffer, sizeof(buffer), stdin)) exit(1);
+        sscanf(buffer, "%9s", pclientRequest->msgData.est.viatura.matricula);
+        valido = strlen(pclientRequest->msgData.est.viatura.matricula) > 0;
+        if (!valido) printf("Campo obrigatório!\n");
+    } while (!valido);
+
+    // País
+    do {
+        printf("País: ");
+        if (!fgets(buffer, sizeof(buffer), stdin)) exit(1);
+        sscanf(buffer, "%2s", pclientRequest->msgData.est.viatura.pais);
+        valido = strlen(pclientRequest->msgData.est.viatura.pais) > 0;
+        if (!valido) printf("Campo obrigatório!\n");
+    } while (!valido);
+
+    // Categoria
+    do {
+        printf("Categoria: ");
+        if (!fgets(buffer, sizeof(buffer), stdin)) exit(1);
+        sscanf(buffer, " %c", &pclientRequest->msgData.est.viatura.categoria);
+        valido = pclientRequest->msgData.est.viatura.categoria != ' ' && pclientRequest->msgData.est.viatura.categoria != '\0';
+        if (!valido) printf("Campo obrigatório!\n");
+    } while (!valido);
+
+    // Nome do Condutor
+    do {
+        printf("Nome do Condutor: ");
+        if (!fgets(buffer, sizeof(buffer), stdin)) exit(1);
+        buffer[strcspn(buffer, "\n")] = 0; // remove newline
+        valido = strlen(buffer) > 0 && strspn(buffer, " ") != strlen(buffer);
+        if (valido) strncpy(pclientRequest->msgData.est.viatura.nomeCondutor, buffer, sizeof(pclientRequest->msgData.est.viatura.nomeCondutor)-1);
+        else printf("Campo obrigatório!\n");
+    } while (!valido);
+
+    pclientRequest->msgData.est.pidCliente = getpid();
+    pclientRequest->msgData.est.pidServidorDedicado = -1;
+
+    so_success("C2.1", "%s %s %c %s %d %d", pclientRequest->msgData.est.viatura.matricula, pclientRequest->msgData.est.viatura.pais, pclientRequest->msgData.est.viatura.categoria, pclientRequest->msgData.est.viatura.nomeCondutor, pclientRequest->msgData.est.pidCliente, pclientRequest->msgData.est.pidServidorDedicado);
     so_debug("> [*pclientRequest:[%s:%s:%c:%s:%d:%d]]", pclientRequest->msgData.est.viatura.matricula, pclientRequest->msgData.est.viatura.pais, pclientRequest->msgData.est.viatura.categoria, pclientRequest->msgData.est.viatura.nomeCondutor, pclientRequest->msgData.est.pidCliente, pclientRequest->msgData.est.pidServidorDedicado);
 }
 
@@ -88,8 +146,12 @@ void c2_1_InputEstacionamento(MsgContent *pclientRequest) {
 void c2_2_EscrevePedido(int msgId, MsgContent clientRequest) {
     so_debug("< [@param msgId:%d, clientRequest:[%s:%s:%c:%s:%d:%d]]", msgId, clientRequest.msgData.est.viatura.matricula, clientRequest.msgData.est.viatura.pais, clientRequest.msgData.est.viatura.categoria, clientRequest.msgData.est.viatura.nomeCondutor, clientRequest.msgData.est.pidCliente, clientRequest.msgData.est.pidServidorDedicado);
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
-
+    clientRequest.msgType = MSGTYPE_LOGIN;
+    if (msgsnd(msgId, &clientRequest, sizeof(MsgContent) - sizeof(long), 0) == -1) {
+        so_error("C2.2", "Erro ao enviar pedido para o servidor");
+        exit(1);
+    }
+    so_success("C2.2", "Pedido enviado com sucesso");
     so_debug(">");
 }
 
@@ -100,7 +162,13 @@ void c2_2_EscrevePedido(int msgId, MsgContent clientRequest) {
 void c3_ProgramaAlarme(int segundos) {
     so_debug("< [@param segundos:%d]", segundos);
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    if (segundos <= 0) {
+        so_error("C3", "Valor de segundos inválido para o alarme");
+        exit(1);
+    }
+    alarm(segundos);
+
+    so_success("C3", "Espera resposta em %d segundos", segundos);
 
     so_debug(">");
 }
@@ -113,7 +181,21 @@ void c3_ProgramaAlarme(int segundos) {
 void c4_1_EsperaRespostaServidor(int msgId, MsgContent *pclientRequest) {
     so_debug("< [@param msgId:%d]", msgId);
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    if (msgrcv(msgId, pclientRequest, sizeof(MsgContent) - sizeof(long), getpid(), 0) == -1) {
+        so_error("C4.1", "Erro ao receber resposta do servidor");
+        exit(1);
+    }
+
+    // Verifica o status da resposta
+    if (pclientRequest->msgData.status == CLIENT_ACCEPTED) {
+        so_success("C4.1", "Check-in realizado com sucesso");
+    } else if (pclientRequest->msgData.status == ESTACIONAMENTO_TERMINADO) {
+        so_success("C4.1", "Não é possível estacionar");
+        exit(0);
+    } else {
+        so_error("C4.1", "Resposta inesperada do servidor");
+        exit(1);
+    }
 
     so_debug("> [*pclientRequest:[%s:%s:%c:%s:%d:%d]]", pclientRequest->msgData.est.viatura.matricula, pclientRequest->msgData.est.viatura.pais, pclientRequest->msgData.est.viatura.categoria, pclientRequest->msgData.est.viatura.nomeCondutor, pclientRequest->msgData.est.pidCliente, pclientRequest->msgData.est.pidServidorDedicado);
 }
@@ -124,7 +206,8 @@ void c4_1_EsperaRespostaServidor(int msgId, MsgContent *pclientRequest) {
 void c4_2_DesligaAlarme() {
     so_debug("<");
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    alarm(0); // Desliga o alarme
+    so_success("C4.2", "Desliguei alarme");
 
     so_debug(">");
 }
@@ -137,7 +220,24 @@ void c4_2_DesligaAlarme() {
 void c5_MainCliente(int msgId, MsgContent *pclientRequest) {
     so_debug("< [@param msgId:%d]", msgId);
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    while (1) {
+        // Espera por mensagem do servidor dedicada ao PID do cliente
+        if (msgrcv(msgId, pclientRequest, sizeof(MsgContent) - sizeof(long), getpid(), 0) == -1) {
+            so_error("C5", "Erro ao receber mensagem do servidor");
+            exit(1);
+        }
+
+        if (pclientRequest->msgData.status == INFO_TARIFA) {
+            so_success("C5", "%s", pclientRequest->msgData.infoTarifa);
+            continue; // Volta ao início do ciclo
+        } else if (pclientRequest->msgData.status == ESTACIONAMENTO_TERMINADO) {
+            so_success("C5", "Estacionamento terminado");
+            exit(0);
+        } else {
+            so_error("C5", "Status inesperado na mensagem");
+            exit(1);
+        }
+    }
 
     so_debug("> [*pclientRequest:[%s:%s:%c:%s:%d:%d]]", pclientRequest->msgData.est.viatura.matricula, pclientRequest->msgData.est.viatura.pais, pclientRequest->msgData.est.viatura.categoria, pclientRequest->msgData.est.viatura.nomeCondutor, pclientRequest->msgData.est.pidCliente, pclientRequest->msgData.est.pidServidorDedicado);
 }
@@ -149,7 +249,18 @@ void c5_MainCliente(int msgId, MsgContent *pclientRequest) {
 void c6_TrataCtrlC(int sinalRecebido) {
     so_debug("< [@param sinalRecebido:%d, msgId:%d, clientRequest:[%s:%s:%c:%s:%d:%d]]", sinalRecebido, msgId, clientRequest.msgData.est.viatura.matricula, clientRequest.msgData.est.viatura.pais, clientRequest.msgData.est.viatura.categoria, clientRequest.msgData.est.viatura.nomeCondutor, clientRequest.msgData.est.pidCliente, clientRequest.msgData.est.pidServidorDedicado);
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    MsgContent terminaMsg;
+    terminaMsg.msgType = clientRequest.msgData.est.pidServidorDedicado;
+    terminaMsg.msgData.status = TERMINA_ESTACIONAMENTO;
+    terminaMsg.msgData.est = clientRequest.msgData.est;
+
+    if (msgsnd(msgId, &terminaMsg, sizeof(MsgContent) - sizeof(long), 0) == -1) {
+        so_error("C6", "Erro ao enviar mensagem de término ao servidor");
+        exit(1);
+    }
+
+    so_success("C6", "Cliente: Shutdown");
+
 
     so_debug(">");
 }
@@ -161,7 +272,8 @@ void c6_TrataCtrlC(int sinalRecebido) {
 void c7_TrataAlarme(int sinalRecebido) {
     so_debug("< [@param sinalRecebido:%d]", sinalRecebido);
 
-    // Substituir este comentário pelo código da função a ser implementado pelo aluno
+    so_error("C7", "Cliente: Timeout");
+    exit(0);
 
     so_debug(">");
 }
